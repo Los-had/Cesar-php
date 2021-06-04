478f24b48dc10df127618165037c798862e0c153
Los-had <githubtestefinal@gmail.com>
2021-06-03 21:41:40 -0300

    crack criado

---
+++ src/cesar_crack.php
@@ -0,0 +1,192 @@
+<?php
+
+// o programa está mal feito por enquanto, futuras atualizações vindo
+
+function cesar_crack($ut) {
+    $txt = $ut;
+    $ftxt = '';
+    $v = str_split($txt);
+    $len = strlen($txt);
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 1;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 2;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 3;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+    
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 4;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 5;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 6;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 7;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 8;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 9;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 10;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 11;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 12;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 13;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 14;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 15;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 16;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 17;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 18;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 19;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 20;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 21;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 22;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 23;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 24;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 25;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+
+    for ($g = 0; $g <= $len; $g++) {
+        $c = ord(@$v[$g]) - 26;
+        $ftxt = $ftxt.chr($c);
+    }
+
+    echo $ftxt . "<br>";
+}
