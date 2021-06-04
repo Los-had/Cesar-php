1281265184265790a534bac3fad6601292b5efdb
Los-had <githubtestefinal@gmail.com>
2021-06-03 21:45:42 -0300

    demonstrando como usar o cesar_crack

--- src/example.php
+++ src/example.php
@@ -3,10 +3,15 @@
 // Adicionando o programa
 require_once 'index.php';
 
+// Adicionando o programa que quebra a criptografia na força bruta
+require_once 'cesar_crack.php';
+
 // Definindo os textos que serão utilizados
 $textoum = 'Olá, tudo bem?';
 $textodois = 'Olá, tudo bem?';
+$textotres = 'RoƤ/#wxgr#ehpB';
 
 // Imprimindo o resultado na tela
 echo cesar($textoum, 3) . "<br>";
-echo cesar_ascii($textodois, 3) ;
+echo cesar_ascii($textodois, 3) . "<br>" ;
+echo cesar_crack($textotres);
