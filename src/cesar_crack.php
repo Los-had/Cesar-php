317576618097b9af9267399f68d3db0ce5a383dc
Los-had <githubtestefinal@gmail.com>
2021-06-04 12:07:23 -0300

    mudanças no cesar_crack
    função cesar_crack adicionada e cesar_crack antiga renomeada para cesar_crack_ascii (pois usa a tabela ascii)

--- src/cesar_crack.php
+++ src/cesar_crack.php
@@ -1,11 +1,10 @@
 <?php
 
-function cesar_crack($ut) {
+function cesar_crack_ascii($ut) {
     $txt = $ut;
     $ftxt = '';
     $v = str_split($txt);
     $len = strlen($txt);
-
     for ($nc = 0; $nc <= 26; $nc++) {
         for ($g = 0; $g <= $len; $g++) {
             $c = ord(@$v[$g]) - $nc;
@@ -15,3 +14,38 @@ function cesar_crack($ut) {
         echo $ftxt . "<br>";
     }
 }
+
+function cesar_crack($string) {
+    for ($nivel = 0; $nivel <= 26; $nivel++) {
+        $m[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $m[] = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz';
+        $m[] = '01234567890123456789';
+        $m[] = 'ÁáÀáÃãÂâÉéÈèÊêÑñÓóÒòÕõÔô';
+        $r = '';
+        for ($a = 0; $a < 2; $a++) { 
+            if (!empty($r)) { 
+                $string = $r;
+                $r = '';
+            }
+            for ($i = 0; $i < strlen($string); $i++) { 
+                $posm = strpos($m[$a],$string[$i]); 
+                if (is_numeric($posm)) { 
+                    $s = $posm + $nivel;
+                    if ($s <= 0) {
+                        $n = 26;
+                        if ($a == 2) {
+                            $n = 10;
+                        }
+                        $s = $s + $n;
+                    }
+                    $r.=$m[$a][$s];
+                } else {
+                    $r.=$string[$i];
+                }
+            }
+        }
+        echo $r . "<br>";
+    }
+}
+
+cesar_crack('rl');
